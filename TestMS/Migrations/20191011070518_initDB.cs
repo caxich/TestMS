03e1801ddf896ed9ebaec58b0227ba99968eab83
晓程 曹 <caxich@hotmail.com>
2019-10-11 15:07:08 +0800

    数据库初始化

---
+++ TestMS/Migrations/20191011070518_initDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TestMS.Migrations
+{
+    public partial class initDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TMS_Menus",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false),
+                    Title = table.Column<string>(maxLength: 50, nullable: false),
+                    Icon = table.Column<string>(maxLength: 50, nullable: true),
+                    Href = table.Column<string>(maxLength: 200, nullable: false),
+                    Spread = table.Column<bool>(nullable: false),
+                    ParentId = table.Column<long>(nullable: false),
+                    Sort = table.Column<int>(nullable: false),
+                    Status = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TMS_Menus", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TMS_Menus");
+        }
+    }
+}
