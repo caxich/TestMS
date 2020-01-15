e691a2837795f940f6c374429b56636981be68bd
caxich <746135024@qq.com>
2020-01-15 15:17:18 +0800

    添加tab页右上角操作按钮功能

--- TestMS/wwwroot/testms/js/cmsIndex.js
+++ TestMS/wwwroot/testms/js/cmsIndex.js
@@ -1,4 +1,5 @@
-﻿layui.use(['form', 'element', 'layer', 'jquery'], function () {
+﻿var menu = [];
+layui.use(['form', 'element', 'layer', 'jquery'], function () {
     var form = layui.form,
         element = layui.element;
     $ = layui.$;
@@ -59,7 +60,7 @@
     })
 
     // 添加新窗口
-    $("body").on("click", ".layui-nav .layui-nav-item a", function () {
+    $("body").on("click", ".navBar .layui-nav .layui-nav-item a", function () {
         addTab($(this));
     })
 
@@ -68,6 +69,10 @@
     var tabIdIndex = 0;
     var layid;
     function addTab(_this) {
+        if (window.sessionStorage.getItem("menu")) {
+            menu = JSON.parse(window.sessionStorage.getItem("menu"));
+        }
+
         tabIdIndex++;
         layid = new Date().getTime();
         var tabtitle = '<i class="layui-icon">' + _this.find("i.layui-icon").text() + '</i>';
@@ -78,11 +83,84 @@
             content: "<iframe src='" + _this.attr("data-url") + "' data-id='" + tabIdIndex + "'></iframe>",
             id: layid
         });
+
+        //缓存当前tab页信息
+        var currentMenu = {
+            "icon": _this.find("i.layui-icon").text(),
+            "title": _this.find("cite").text(),
+            "href": _this.attr("data-url"),
+            "layId": new Date().getTime()
+        }
+        menu.push(currentMenu);
+        window.sessionStorage.setItem("menu", JSON.stringify(menu)); //打开的窗口
         element.tabChange('bodyTab', layid);
     }
 
     $("body").on("click", ".top_tab li i.layui-tab-close", function () {
         element.tabDelete("bodyTab", $(this).parent("li").attr("lay-id")).init();
+    })
+
+    //刷新当前
+    $(".refresh").on("click", function () {  //此处添加禁止连续点击刷新一是为了降低服务器压力，另外一个就是为了防止超快点击造成chrome本身的一些js文件的报错(不过貌似这个问题还是存在，不过概率小了很多)
+        if ($(this).hasClass("refreshThis")) {
+            $(this).removeClass("refreshThis");
+            $(".clildFrame .layui-tab-item.layui-show").find("iframe")[0].contentWindow.location.reload();
+            setTimeout(function () {
+                $(".refresh").addClass("refreshThis");
+            }, 2000)
+        } else {
+            layer.msg("您点击的速度超过了服务器的响应速度，还是等两秒再刷新吧！");
+        }
+    })
+
+    //关闭其他
+    $(".closePageOther").on("click", function () {
+        if ($("#top_tabs li").length > 2 && $("#top_tabs li.layui-this cite").text() != "后台首页") {
+            var menu = JSON.parse(window.sessionStorage.getItem("menu"));
+            $("#top_tabs li").each(function () {
+                if ($(this).attr("lay-id") != '' && !$(this).hasClass("layui-this")) {
+                    element.tabDelete("bodyTab", $(this).attr("lay-id")).init();
+                    //此处将当前窗口重新获取放入session，避免一个个删除来回循环造成的不必要工作量
+                    for (var i = 0; i < menu.length; i++) {
+                        if ($("#top_tabs li.layui-this cite").text() == menu[i].title) {
+                            menu.splice(0, menu.length, menu[i]);
+                            window.sessionStorage.setItem("menu", JSON.stringify(menu));
+                        }
+                    }
+                }
+            })
+        } else if ($("#top_tabs li.layui-this cite").text() == "后台首页" && $("#top_tabs li").length > 1) {
+            $("#top_tabs li").each(function () {
+                if ($(this).attr("lay-id") != '' && !$(this).hasClass("layui-this")) {
+                    element.tabDelete("bodyTab", $(this).attr("lay-id")).init();
+                    window.sessionStorage.removeItem("menu");
+                    menu = [];
+                    window.sessionStorage.removeItem("curmenu");
+                }
+            })
+        } else {
+            layer.msg("没有可以关闭的窗口了@_@");
+        }
+        //渲染顶部窗口
+        tab.tabMove();
+    })
+
+    //关闭全部
+    $(".closePageAll").on("click", function () {
+        if ($("#top_tabs li").length > 1) {
+            $("#top_tabs li").each(function () {
+                if ($(this).attr("lay-id") != '') {
+                    element.tabDelete("bodyTab", $(this).attr("lay-id")).init();
+                    window.sessionStorage.removeItem("menu");
+                    menu = [];
+                    window.sessionStorage.removeItem("curmenu");
+                }
+            })
+        } else {
+            layer.msg("没有可以关闭的窗口了@_@");
+        }
+        //渲染顶部窗口
+        tab.tabMove();
     })
 
 })
